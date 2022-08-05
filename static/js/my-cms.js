ede14f5e4b7a0f48f57dcc858cee88a682605603
zealzel <zealzel@gmail.com>
2022-08-05 17:16:49 +0800

    Modified.

--- static/js/my-cms.js
+++ static/js/my-cms.js
@@ -1,4 +1,6 @@
-import CMS from "netlify-cms";
+//import CMS from "netlify-cms-app";
+//import CMS from "node_modules/netlify-cms/dist/netlify-cms.js";
+//const CMS = require("netlify-cms");
 // This global flag enables manual initialization.
 //window.CMS_MANUAL_INIT = true
 // Usage with import from npm package
@@ -9,36 +11,37 @@ import CMS from "netlify-cms";
  * Netlify CMS the old way.
  */
 //init()
-CMS.init();
+console.log("GGG");
+//CMS.init();
 
 
-if (window.netlifyIdentity) {
-  console.log("AAA");
-  window.netlifyIdentity.on("init", (user) => {
-    console.log("BBB");
-    console.log(user);
-    if (!user) {
-      console.log("CCC");
-      window.netlifyIdentity.on("", () => {
-        console.log("UUU");
-      });
-      window.netlifyIdentity.on("/", () => {
-        console.log("DDD");
-      });
-      window.netlifyIdentity.on("login", (user) => {
-        console.log("EEE");
-        console.log(user);
-      });
-    }
-  });
-}
+//if (window.netlifyIdentity) {
+  //console.log("AAA");
+  //window.netlifyIdentity.on("init", (user) => {
+    //console.log("BBB");
+    //console.log(user);
+    //if (!user) {
+      //console.log("CCC");
+      //window.netlifyIdentity.on("", () => {
+        //console.log("UUU");
+      //});
+      //window.netlifyIdentity.on("/", () => {
+        //console.log("DDD");
+      //});
+      //window.netlifyIdentity.on("login", (user) => {
+        //console.log("EEE");
+        //console.log(user);
+      //});
+    //}
+  //});
+//}
 
-CMS.registerEventListener({
-  name: "preSave",
-  handler: ({ entry }) => {
-    console.log("========= author !!!!!!!!!!!!!!!!!!!!!!!");
-    const author = JSON.parse(localStorage.getItem("netlify-cms-user")).name;
-    console.log(author);
-    return entry.get("data").set("author", author);
-  },
-});
+//CMS.registerEventListener({
+  //name: "preSave",
+  //handler: ({ entry }) => {
+    //console.log("========= author !!!!!!!!!!!!!!!!!!!!!!!");
+    //const author = JSON.parse(localStorage.getItem("netlify-cms-user")).name;
+    //console.log(author);
+    //return entry.get("data").set("author", author);
+  //},
+//});
